ff13146ec1f745933dd4bdf0a6c0829ccaa7a9aa
Enoch2k2 <enoch2k2@hotmail.com>
2018-07-16 11:38:40 -0500

    updated Garfield to Grumpy Cat

--- insert_pets.sql
+++ insert_pets.sql
@@ -1,5 +1,5 @@
 INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Felix", 2, "Cat", 5000, 1);
-INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Garfield", 8, "Cat", 120000, 1);
+INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Grumpy Cat", 8, "Cat", 120000, 1);
 INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Shadow", 5, "Cat", 500, 2);
 INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Fido", 1, "Dog", 250000, 2);
 INSERT INTO pets (name, age, species, net_worth, owner_id) VALUES ("Lassie", 9, "Dog", 1000000, NULL);
